cf2b0f0c5eb40c9b8142924efb3a0c108298d6d6
d00d1e <lee.julianay@gmail.com>
2020-11-21 20:53:59 -0800

    create CartView, add-to-cart fxn/redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import CartView from './views/CartView';
 import HomeView from './views/HomeView';
 import ProductView from './views/ProductView';
 
 function App() {
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">folia<i className="fab fa-pagelines"></i></a>
+            <Link className="brand" to="/">folia<i className="fab fa-pagelines"></i></Link>
           </div>
           <div id="links">
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
-          <Route exact path="/" component={HomeView}></Route>
-          <Route path="/product/:id" component={ProductView}></Route>
+          <Route exact path="/" component={HomeView} />
+          <Route path="/product/:id" component={ProductView} />
+          <Route path="/cart/:id?" component={CartView} />
         </main>
         <footer className="row center">&#169;2020 All rights reserved</footer>
       </div>
