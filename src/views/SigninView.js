fbad3a64abd05e2a265fdcba8d77009c718c8db9
d00d1e <lee.julianay@gmail.com>
2020-11-24 17:15:51 -0800

    implement signin/signout functionality

--- src/views/SigninView.js
+++ src/views/SigninView.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { signin } from '../actions/userActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 
-export default function SigninView() {
+export default function SigninView(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const redirect = props.location.search ? props.location.search.split('=')[1] : '/'
+
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo, loading, error } = userSignin;
+
+  const dispatch = useDispatch();
   const submitHandler = (e) => {
     e.preventDefault();
-    // signin fxn
-  }
+    dispatch(signin(email, password));
+  };
+
+  useEffect(() => {
+    if(userInfo){
+      props.history.push(redirect);
+    }
+  }, [props.history, redirect, userInfo]);
 
   return (
     <div>
@@ -17,6 +33,8 @@ export default function SigninView() {
         <div className="form-title">
           <h1>Sign In</h1>
         </div>
+        {loading && <LoadingBox />}
+        {error && <MessageBox variant="danger">{error}</MessageBox> }
         <div>
           <label htmlFor="email">Email</label>
           <input
