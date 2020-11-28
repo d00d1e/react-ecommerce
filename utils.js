f133c735a8ce39658c92ff7b2fa5c7adaed64c95
d00d1e <lee.julianay@gmail.com>
2020-11-27 18:57:31 -0800

    create orderRouter

--- utils.js
+++ utils.js
@@ -14,3 +14,23 @@ export const generateToken = (user) => {
     }
   );
 };
+
+// authenticate user request
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || 'myjwtsecret', (err, decode) => {
+        if(err) {
+          res.status(401).send({ message: 'Invalid token' });
+        } else {
+          res.user = decode
+        }
+      }
+    );
+  } else {
+    res.status(401).send({ message: 'No token' });
+  }
+};
