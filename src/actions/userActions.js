50ff10fc60574d0419758e152cf5ff5773d7f655
d00d1e <lee.julianay@gmail.com>
2020-12-01 13:02:47 -0800

    Create ProfileView

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
-import { USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT } from '../constants/userConstants';
+import { USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT, USER_DETAILS_REQUEST, USER_DETAILS_FAIL, USER_DETAILS_SUCCESS } from '../constants/userConstants';
 
 export const signin = (email, password) => async(dispatch) => {
   dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
@@ -46,3 +46,20 @@ export const signout = () => (dispatch) => {
   // document.location.href = '/signin';
 };
 
+export const detailsUser = (userId) => async (dispatch, getState) => {
+  dispatch({ type: USER_DETAILS_REQUEST, payload: userId });
+  const { userSignin: { userInfo } } = getState();
+
+  try {
+    const { data } = await Axios.get(`/api/users/${userId}`, {
+      headers: { Authorization: `Bearer ${userInfo.token}` },
+    });
+    dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+  } catch (err) {
+    const message =
+      err.response && err.response.data.message
+        ? err.response.data.message
+        : err.message;
+    dispatch({ type: USER_DETAILS_FAIL, payload: message });
+  }
+};
